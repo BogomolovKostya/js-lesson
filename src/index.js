e0aaaeb411bf635892c6bd0ff99ad31a946f35c2
Kostya Bogomolov <webarchitectors@gmail.com>
2017-12-09 03:48:30 +0300

    lesson6

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
+
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
+ */
+function delayPromise(seconds) {
+    return new Promise(function(resolve) {
+        seconds = 1000;
+        setTimeout(resolve, seconds);
+    });
+}
+
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
+ */
+function loadAndSortTowns() {
+    return new Promise(function(resolve) {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.responseType = 'json';
+
+        xhr.addEventListener('load', function() {
+            resolve(xhr.response.sort(function(a, b) {
+                return (a.name > b.name) ? 1 : (a.name < b.name) ? -1 : 0;
+            }));
+        });
+
+        xhr.send();
+    })
+}
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
